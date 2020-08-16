1743fcb4b0bc65d426e176e29d219db795d0676a
haden <haden.lee@molocoads.com>
2020-08-16 15:57:33 -0700

    clean up

--- src/main/java/edu/usfca/cs673/Task2.java
+++
@@ -1,9 +0,0 @@
-package edu.usfca.cs673;
-
-public class Task2 {
-  public static int getNumberOfInversions(int[] A) {
-    // TODO: Implement this.
-    int n = A.length, cnt = 0;
-    return cnt;
-  }
-}
--- src/main/java/edu/usfca/cs673/Task4.java
+++
@@ -1,8 +0,0 @@
-package edu.usfca.cs673;
-
-public class Task4 {
-  public static double getProbability(int n, int k) {
-    // TODO: Implement this.
-    return 0.0;
-  }
-}
